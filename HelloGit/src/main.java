37e318042eaeb82c20f645688bc98f05ab401c53
kracht <0kracht@informatik.uni-hamburg.de>
2013-08-13 09:37:16 +0200

    65

--- HelloGit/src/main.java
+++ HelloGit/src/main.java
@@ -3,8 +3,9 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.format("Hello Git %d!\n", 4);
-		
+		System.out.format("Hello Git!\n");
+		//sdsasadsadsdasda
+		// fbvcv
 	}
 
 }
